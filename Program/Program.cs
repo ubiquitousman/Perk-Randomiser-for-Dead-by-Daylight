7db993114e9218b93c5531351e103841a2d735b8
Alexander <alexandersvanholm@outlook.com>
2020-02-09 08:08:49 +0100

    Reorganisation of files

--- Program/Program.cs
+++ Program/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-public class Program
+static class Program
 {
 	static void Main(){
         Application.EnableVisualStyles();
